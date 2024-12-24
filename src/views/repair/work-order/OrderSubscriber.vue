c2eb811680708ad64c04a1eb50eadcf799693787
HK-XXB-张尧 <HK-XXB-@tencent.com>
2024-12-24 15:21:38 +0800

    fix: 工单推送对象的 select value 字段选择错误

--- src/views/repair/work-order/OrderSubscriber.vue
+++ src/views/repair/work-order/OrderSubscriber.vue
@@ -6,13 +6,18 @@ vue
       v-model="subscribers"
       class="w-60%!"
       multiple
-      clearable
+      filterable
       collapse-tags
       collapse-tags-tooltip
       :max-collapse-tags="4"
       value-key="value"
     >
-      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item" />
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
     </el-select>
     <template #footer>
       <el-button @click="submitForm" type="primary">确定</el-button>
@@ -29,7 +34,7 @@ defineOptions({ name: 'OrderSubscriber' })
 const message = useMessage()
 const employeeStore = useEmployeeStoreWithOut()
 
-const subscribers = ref<OptionItem<number>[]>([])
+const subscribers = ref<number[]>([])
 const options = computed(() => {
   return employeeStore.infoEmployees.map((item) => ({ value: item.id, label: item.nickname }))
 })
@@ -39,7 +44,7 @@ const open = async () => {
   subscribers.value = []
   visible.value = true
   const data = await getOrderSubscriber()
-  subscribers.value = data.map((item) => ({ value: item.id, label: item.nickname }))
+  subscribers.value = data.map((item) => item.userId)
 }
 defineExpose({ open })
 
@@ -47,7 +52,7 @@ const loading = ref(false)
 const submitForm = async () => {
   loading.value = true
   try {
-    await setOrderSubscriber({ userId: subscribers.value.map((s) => s.value) })
+    await setOrderSubscriber({ userId: subscribers.value })
     message.success('推送对象设置成功')
     visible.value = false
   } finally {
