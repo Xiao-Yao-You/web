5ab8e39a53b7baed2e213ccd9a63ccb5a5d0f398
HK-XXB-张尧 <HK-XXB-@tencent.com>
2025-02-13 18:09:08 +0800

    feat: 添加工单流程时间线

--- src/views/repair/work-order/OrderDetail.vue
+++ src/views/repair/work-order/OrderDetail.vue
@@ -3,41 +3,80 @@
     title="工单详情"
     v-model="visible"
     scroll
-    maxHeight="500px"
+    maxHeight="60vh"
     :close-on-click-modal="false"
     :close-on-press-escape="false"
   >
-    <el-descriptions :column="1" border>
-      <el-descriptions-item label="工单编号" :width="10"> {{ info.code }} </el-descriptions-item>
-      <el-descriptions-item label="工单标题"> {{ info.title }} </el-descriptions-item>
-      <el-descriptions-item label="二维码编号"> {{ info.labelCode }} </el-descriptions-item>
-      <el-descriptions-item label="设备名称"> {{ info.deviceName }} </el-descriptions-item>
-      <el-descriptions-item label="设备所在地点"> {{ info.address }} </el-descriptions-item>
-      <el-descriptions-item label="设备具体位置">{{ info.location }}</el-descriptions-item>
-      <el-descriptions-item label="报修人"> {{ info.submitUserNickName }} </el-descriptions-item>
-      <el-descriptions-item label="报修人电话"> {{ info.submitUserMobile }} </el-descriptions-item>
-      <el-descriptions-item label="请求类型">
-        {{ getDictLabel(DICT_TYPE.REPAIR_REQUEST_TYPE, info.requestType) }}
-      </el-descriptions-item>
-      <el-descriptions-item label="问题类型">{{ info.questionTypeStr }}</el-descriptions-item>
-      <el-descriptions-item label="紧急程度">
-        {{ getDictLabel(DICT_TYPE.LEVEL, info.level) }}
-      </el-descriptions-item>
-      <el-descriptions-item label="问题描述"> {{ info.description }} </el-descriptions-item>
-      <el-descriptions-item label="问题图片">
-        <el-empty
-          v-if="!pictures.length"
-          style="padding: unset"
-          :image-size="80"
-          description="暂无数据"
-        />
-        <el-carousel v-else trigger="click">
-          <el-carousel-item v-for="(url, index) in pictures" :key="index">
-            <el-image style="width: 100%; height: 100%" :src="url" fit="contain" />
-          </el-carousel-item>
-        </el-carousel>
-      </el-descriptions-item>
-    </el-descriptions>
+    <el-tabs v-model="activeName" class="demo-tabs">
+      <el-tab-pane label="工单信息" name="info">
+        <el-descriptions :column="1" border>
+          <el-descriptions-item label="工单编号" :width="10">
+            {{ info.code }}
+          </el-descriptions-item>
+          <el-descriptions-item label="工单标题"> {{ info.title }} </el-descriptions-item>
+          <el-descriptions-item label="二维码编号"> {{ info.labelCode }} </el-descriptions-item>
+          <el-descriptions-item label="设备名称"> {{ info.deviceName }} </el-descriptions-item>
+          <el-descriptions-item label="设备所在地点"> {{ info.address }} </el-descriptions-item>
+          <el-descriptions-item label="设备具体位置">{{ info.location }}</el-descriptions-item>
+          <el-descriptions-item label="报修人">
+            {{ info.submitUserNickName }}
+          </el-descriptions-item>
+          <el-descriptions-item label="报修人电话">
+            {{ info.submitUserMobile }}
+          </el-descriptions-item>
+          <el-descriptions-item label="请求类型">
+            {{ getDictLabel(DICT_TYPE.REPAIR_REQUEST_TYPE, info.requestType) }}
+          </el-descriptions-item>
+          <el-descriptions-item label="问题类型">{{ info.questionTypeStr }}</el-descriptions-item>
+          <el-descriptions-item label="紧急程度">
+            {{ getDictLabel(DICT_TYPE.LEVEL, info.level) }}
+          </el-descriptions-item>
+          <el-descriptions-item label="问题描述"> {{ info.description }} </el-descriptions-item>
+        </el-descriptions>
+      </el-tab-pane>
+      <el-tab-pane label="处理流程" name="workflow">
+        <el-timeline>
+          <el-timeline-item
+            v-for="(record, index) in info.recordList"
+            :key="record.id"
+            :timestamp="formatDate(record.createTime)"
+            placement="top"
+          >
+            <el-card body-class="py-10px!">
+              <el-descriptions :column="1">
+                <el-descriptions-item label="处理状态：">
+                  {{ OperateType[record.operateType] }}
+                </el-descriptions-item>
+                <el-descriptions-item :label="`${index ? '处理' : '报修'}人：`">
+                  {{ record.operateUserNickName }}
+                </el-descriptions-item>
+                <el-descriptions-item label="说明：">
+                  {{ record.remark || '无' }}
+                </el-descriptions-item>
+                <el-descriptions-item label="图片：" label-class-name="block">
+                  <el-empty v-if="!record.picture" style="padding: unset" :image-size="50" />
+                  <template v-else>
+                    <el-image
+                      class="mr-10px"
+                      v-for="(url, i) in record.picture.split(';')"
+                      :key="i"
+                      style="width: 100px; height: 100px"
+                      :src="url"
+                      fit="cover"
+                      :zoom-rate="1.2"
+                      :max-scale="7"
+                      :min-scale="0.2"
+                      show-progress
+                      :preview-src-list="record.picture?.split(';')"
+                    />
+                  </template>
+                </el-descriptions-item>
+              </el-descriptions>
+            </el-card>
+          </el-timeline-item>
+        </el-timeline>
+      </el-tab-pane>
+    </el-tabs>
   </Dialog>
 </template>
 
@@ -46,21 +85,18 @@ import { ElLoading } from 'element-plus'
 import { DICT_TYPE, getDictLabel } from '@/utils/dict'
 import { getRepairOrder, type RepairOrder } from '@/api/repair'
 import { OperateType } from '@/api/repair/constant'
+import { formatDate } from '@/utils/formatTime'
 
 defineOptions({
   name: 'OrderDetail'
 })
 
 const visible = ref(false)
+
+const activeName = ref('info') // info-工单信息, workflow-处理流程
+
 const info = ref({} as RepairOrder)
-const pictures = computed(() => {
-  if (Array.isArray(info.value.recordList)) {
-    const record = info.value.recordList.find((item) => item.operateType === OperateType.CHUANGJIAN)
-    return record?.picture ? record.picture.split(';') : []
-  } else {
-    return []
-  }
-})
+
 const open = async (id: number) => {
   info.value = {} as RepairOrder
   visible.value = true
