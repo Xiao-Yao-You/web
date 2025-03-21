9831c4a4e8f35c895c1c964afa7c0b8cbc54b705
HK-XXB-张尧 <HK-XXB-@tencent.com>
2025-03-21 08:33:12 +0800

    refactor: 重构统计概览页

--- src/views/repair/statistics/Individual.vue
+++ src/views/repair/statistics/Individual.vue
@@ -1,100 +1,127 @@
 <template>
   <ContentWrap>
-    <el-date-picker
-      v-model="reportMonth"
-      type="month"
-      value-format="YYYY-MM"
-      placeholder="请选择年月"
-      :disabled-date="handleDisabledDate"
-    />
-    <el-button plain type="success" class="ml-20px" @click="createReport">
-      <Icon class="mr-5px" icon="ep:data-analysis" />
-      生成新报表
-    </el-button>
-    <el-button plain type="primary" class="ml-20px" @click="refreshReport">
-      <Icon class="mr-5px" icon="ep:refresh" />
-      刷新
-    </el-button>
-  </ContentWrap>
-
-  <ContentWrap title="月度报表">
-    <el-table v-loading="loading" :data="list" stripe show-overflow-tooltip>
-      <el-table-column label="月份" prop="reportMonth" />
-      <el-table-column label="报表" prop="reportTitle" />
-      <el-table-column label="操作">
+    <el-table v-loading="loading" :data="data" stripe show-overflow-tooltip highlight-current-row>
+      <el-table-column label="月份" prop="reportMonth" fixed width="90" />
+      <el-table-column label="姓名" prop="username" fixed width="90" />
+      <el-table-column label="总工单数量" prop="totalOrderCount" width="100" align="center">
         <template #default="{ row }">
-          <router-link :to="`/repair/statistics/individual/${row.reportMonth}`">
-            <el-button link type="primary">详情</el-button>
-          </router-link>
-          <!-- <el-button link type="warning">导出</el-button> -->
+          {{ row.totalOrderCount ?? '-' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="已完成数量" prop="completeOrderCount" width="100" align="center">
+        <template #default="{ row }">
+          {{ row.completeOrderCount ?? '--' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="处理中数量" prop="processingOrderCount" width="100" align="center">
+        <template #default="{ row }">
+          {{ row.processingOrderCount ?? '--' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="挂起数量" prop="pendingOrderCount" align="center">
+        <template #default="{ row }">
+          {{ row.pendingOrderCount ?? '--' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="处理总时长" prop="totalHandleTime" width="110" align="center">
+        <template #default="{ row: { totalHandleTime } }">
+          {{ isNullOrUnDef(totalHandleTime) ? '--' : formatMs(totalHandleTime) }}
+        </template>
+      </el-table-column>
+      <el-table-column label="平均处理时长" prop="aht" width="110" align="center">
+        <template #default="{ row: { aht } }">
+          {{ isNullOrUnDef(aht) ? '--' : formatMs(aht) }}
+        </template>
+      </el-table-column>
+      <el-table-column label="处理时长/工作时长" prop="timeProportion" width="150" align="center">
+        <template #default="{ row: { timeProportion } }">
+          {{ isNullOrUnDef(timeProportion) ? '--' : `${timeProportion} %` }}
+        </template>
+      </el-table-column>
+      <el-table-column label="低紧急工单数" prop="lowLevelNum" width="110" align="center" />
+      <el-table-column label="中紧急工单数" prop="middleLevelNum" width="110" align="center" />
+      <el-table-column label="高紧急工单数" prop="highLevelNum" width="110" align="center" />
+      <el-table-column label="主动接单数" prop="scrambleNum" width="110" align="center" />
+      <el-table-column
+        label="主动接单占比"
+        prop="orderAcceptedProportion"
+        width="110"
+        align="center"
+      >
+        <template #default="{ row: { orderAcceptedProportion } }">
+          {{ isNullOrUnDef(orderAcceptedProportion) ? '--' : `${orderAcceptedProportion} %` }}
+        </template>
+      </el-table-column>
+      <el-table-column label="挂起总时长" prop="pendingTotalTime" width="110" align="center">
+        <template #default="{ row: { pendingTotalTime } }">
+          {{ isNullOrUnDef(pendingTotalTime) ? '--' : formatMs(pendingTotalTime) }}
+        </template>
+      </el-table-column>
+      <el-table-column label="平均挂起时长" prop="apt" width="110" align="center">
+        <template #default="{ row: { apt } }">
+          {{ isNullOrUnDef(apt) ? '--' : formatMs(apt) }}
+        </template>
+      </el-table-column>
+      <el-table-column label="按时完成率" prop="onTimeCompletionRate" width="100" align="center">
+        <template #default="{ row: { onTimeCompletionRate } }">
+          {{ isNullOrUnDef(onTimeCompletionRate) ? '--' : `${onTimeCompletionRate} %` }}
+        </template>
+      </el-table-column>
+      <el-table-column label="环比增长量" prop="monthOnMonthGrowth" width="100" align="center">
+        <template #default="{ row }">
+          {{ row.monthOnMonthGrowth ?? '--' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="环比增长率" prop="monthOnMonthGrowthRate" width="100" align="center">
+        <template #default="{ row: { monthOnMonthGrowthRate } }">
+          {{ isNullOrUnDef(monthOnMonthGrowthRate) ? '--' : `${monthOnMonthGrowthRate} %` }}
         </template>
       </el-table-column>
     </el-table>
-    <Pagination
-      :total="total"
-      v-model:page="queryParams.pageNo"
-      v-model:limit="queryParams.pageSize"
-      @pagination="query"
-    />
   </ContentWrap>
 </template>
 
 <script setup lang="ts">
-import { getIndividualPage, createIndividualReport } from '@/api/statistics'
+import { getIndividualReport } from '@/api/statistics'
+import { formatMs } from '@/utils/formatTime'
+import { isNullOrUnDef } from '@/utils/is'
 
 defineOptions({
-  name: 'StatsIndividual'
+  name: 'IndividualReport'
 })
 
-const loading = ref(true)
-const queryParams = reactive({
-  pageNo: 1,
-  pageSize: 20
-})
-const total = ref(0)
-const list = ref<any[]>([])
+const route = useRoute()
+
+const loading = ref(false)
+const data = ref<any[]>([])
 const query = async () => {
-  try {
-    const data = await getIndividualPage(queryParams)
-    list.value = data.list || []
-    total.value = data.total
-  } finally {
-    loading.value = false
+  const { list } = await getIndividualReport({
+    pageNo: 1,
+    pageSize: 50,
+    reportMonth: route.params.month as string
+  })
+  if (Array.isArray(list) && list.length) {
+    data.value = list.map((item) => {
+      // 提取派单、抢单、转单数量和高、中、低紧急工单数量
+      const { orderTypeDistribution, urgencyLevelDistribution, ...rest } = item
+      // prettier-ignore
+      const [dispatchNum = '--', scrambleNum = '--', transferNum = '--'] = (orderTypeDistribution ?? '').match(/\d+/g) || []
+      // prettier-ignore
+      const [lowLevelNum = '--', middleLevelNum = '--', highLevelNum = '--'] = (urgencyLevelDistribution ?? '').match(/\d+/g) || []
+
+      return {
+        ...rest,
+        lowLevelNum,
+        middleLevelNum,
+        highLevelNum,
+        dispatchNum,
+        scrambleNum,
+        transferNum
+      }
+    })
   }
 }
-
-// 当月即之后的月份禁用
-const handleDisabledDate = (time: Date) => {
-  const currentDate = new Date()
-  const currentMonth = currentDate.getMonth()
-  const currentYear = currentDate.getFullYear()
-
-  const selectedMonth = time.getMonth()
-  const selectedYear = time.getFullYear()
-
-  // prettier-ignore
-  return selectedYear > currentYear || (selectedYear === currentYear && selectedMonth >= currentMonth)
-}
-
-//#region 生成统计报告
-const reportMonth = ref(undefined)
-const createReport = async () => {
-  try {
-    loading.value = true
-    await createIndividualReport(reportMonth.value)
-    await query()
-  } finally {
-    loading.value = false
-  }
-}
-
-// 刷新
-const refreshReport = async () => {
-  queryParams.pageNo = 1
-  queryParams.pageSize = 20
-  reportMonth.value = undefined
-  await query()
-}
+defineExpose({ query })
 
 onMounted(() => {
   query()
--- src/views/repair/statistics/index.vue
+++ src/views/repair/statistics/index.vue
@@ -1,16 +1,15 @@
 <template>
   <el-tabs v-model="activeTab">
-    <el-tab-pane label="人工统计" name="individual">
-      <Individual />
-    </el-tab-pane>
+    <el-tab-pane label="人工统计" name="individual" />
     <el-tab-pane label="工单统计" name="order" />
     <el-tab-pane label="资产统计" name="assets" />
     <el-tab-pane label="异常统计" name="exception" />
   </el-tabs>
+  <Overview key="exception" :tab="activeTab" />
 </template>
 
 <script setup lang="ts">
-import Individual from './Individual.vue'
+import Overview from './Overview.vue'
 
 defineOptions({ name: 'RepairStatistics' })
 
