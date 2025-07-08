f5bc352b24a331358d2ca2acdfe65da82ccb4da7
zhangYao719 <1258858403@qq.com>
2025-07-08 10:28:35 +0800

    wip: 统计页面概览

---
+++ src/api/statistics/group-order.ts
@@ -0,0 +1,32 @@
+import request from '@/config/axios'
+
+// 分页查询所有的分组工单统计列表
+export const getGroupPage = (params: Required<PageParam>) => {
+  return request.get<{ total: number; list: any[] }>({
+    url: '/report-group-order/page',
+    params
+  })
+}
+
+/**
+ * @description 创建某月的分组工单统计
+ * @param {string} month 年月，比如 '2025-01'
+ */
+export const createGroupReport = (month?: string) => {
+  return request.post({
+    url: `/report-group-order/generateReport?month=${month}`
+  })
+}
+
+export interface GroupParams extends Required<PageParam> {
+  month: string // 年月，比如 '2025-03'
+  groupId: string // 分组id
+}
+
+// 查询某月的分组工单统计
+export const getGroupReport = (params: GroupParams) => {
+  return request.get<{ list: any[]; total: number }>({
+    url: '/report-group-order-detail/page',
+    params
+  })
+}
--- src/views/repair/statistics/index.vue
+++ src/views/repair/statistics/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-tabs v-model="activeTab">
     <el-tab-pane label="人工统计" name="individual" />
-    <el-tab-pane label="工单统计" name="order" />
+    <el-tab-pane label="工单统计" name="groupOrder" />
     <el-tab-pane label="资产统计" name="assets" />
     <el-tab-pane label="异常统计" name="exception" />
   </el-tabs>
